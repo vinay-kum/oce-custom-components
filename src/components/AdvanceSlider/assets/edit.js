5919c21040a76f275c77d7f6acf1915ea28451f6
Vinay Kumar <vinaykumar@Vinays-MacBook-Pro.local>
2020-04-28 17:00:50 +0530

    twitter component added

---
+++ src/components/AdvanceSlider/assets/edit.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+/* globals define */
+
+define([
+	'jquery'
+], function( $ ) {
+	'use strict';
+
+	function EditHandlers( params ) {
+		this.componentId = params.componentId;
+	}
+
+	EditHandlers.prototype = {
+
+		onDragOver: function( evt, dataTransfer ) {
+			var response = true; // Allow the drop
+
+			// Only allow the designated number of child components to
+			// be added to this component.
+			var slData = SCSRenderAPI.getComponentInstanceData( this.componentId );
+			if( slData && slData.customSettingsData &&
+				( typeof slData.customSettingsData.maxItems === 'number' ) &&
+				( slData.customSettingsData.maxItems > 0 ) ) {
+
+				if( Array.isArray( slData.components ) &&
+					( slData.components.length >= slData.customSettingsData.maxItems ) ) {
+					// To prevent the drop, return a string.  A non-empty string
+					// displays in a tooltip.  An empty string just prevents the drop.
+					response = 'Maximum Items: ' + slData.customSettingsData.maxItems;
+				}
+			}
+
+			return response;
+		}
+
+	};
+
+	return EditHandlers;
+});
